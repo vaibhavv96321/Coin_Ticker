27cc24204bff231e946944406327531717331043
Beast <vaibhavv96321@gmail.com>
2021-08-18 17:22:51 +0530

    Linked the online api to the visible app and also made 3 columns for other data

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,20 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const kApikey = '7F66F50F-0BF0-4312-9C22-004086A23B6F';
+const kUrl = 'https://rest.coinapi.io/v1/exchangerate/';
+
+class CoinData {
+  double exchangeRate;
+  Future<double> getNetworkInfo(String curCur) async {
+    var url = http.get('${kUrl}BTC/$curCur?apikey=$kApikey');
+    http.Response response = await url;
+    if (response.statusCode == 200) {
+      exchangeRate = jsonDecode(response.body)['rate'];
+    } else {
+      print(response.statusCode);
+      throw ('not possible to proceed');
+    }
+    return exchangeRate;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,38 +1,17 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
 import 'dart:io' show Platform;
-import 'package:http/http.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
-const kApikey = '7F66F50F-0BF0-4312-9C22-004086A23B6F';
-double exchaneRate = 0.0;
-
-Future<double> getNetworkInfo() async {
-  var url =
-      get('https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=$kApikey');
-  Response response = await url;
-  if (response.statusCode == 200) {
-    String body = response.body;
-    var apiData = jsonDecode(body);
-    exchaneRate = apiData['rate'];
-  } else {
-    print(response.statusCode);
-    exchaneRate = 0;
-  }
-  return await exchaneRate;
-}
-
 class _PriceScreenState extends State<PriceScreen> {
-  String currentCurrency = "USD";
+  String currentCurrency = "INR";
 
-  DropdownButton androidDropButton() {
+  DropdownButton dropButton() {
     List<DropdownMenuItem> currencies = [];
     for (String cur in currenciesList) {
       Widget currency = DropdownMenuItem(
@@ -47,12 +26,13 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           currentCurrency = value;
+          getData(value);
         });
       },
     );
   }
 
-  CupertinoPicker iosPicker() {
+  CupertinoPicker sliderPicker() {
     List<Widget> currencies = [];
     for (String cur in currenciesList) {
       Widget currency = Text(cur);
@@ -63,20 +43,30 @@ class _PriceScreenState extends State<PriceScreen> {
       backgroundColor: Colors.lightBlue,
       itemExtent: 32,
       onSelectedItemChanged: (selectedItem) {
-        print(selectedItem);
+        currentCurrency = currenciesList[selectedItem];
+        getData(currenciesList[selectedItem]);
       },
       children: currencies,
     );
   }
 
-  void testing() async {
-    double exchange = await getNetworkInfo();
-    print(exchange);
+  String bitcoinValue = '?';
+
+  void getData(String diffCur) async {
+    try {
+      double data = await CoinData().getNetworkInfo(diffCur);
+      setState(() {
+        bitcoinValue = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   void initState() {
-    testing();
+    super.initState();
+    getData('INR');
   }
 
   @override
@@ -100,7 +90,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinValue $currentCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -110,12 +100,57 @@ class _PriceScreenState extends State<PriceScreen> {
               ),
             ),
           ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = $bitcoinValue $currentCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = $bitcoinValue $currentCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 300,
+          ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: Platform.isIOS ? iosPicker() : androidDropButton(),
+            child: Platform.isAndroid ? sliderPicker() : dropButton(),
           ),
         ],
       ),
